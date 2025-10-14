41cd3bae5bb098bff5fd4f5b1dc249753372f9c8
Samuferi <fecoo2005@gmail.com>
2025-10-14 17:53:34 +0200

    hibajavitasok

--- frontend/scripts/uj_munkatars_valid.js
+++ frontend/scripts/uj_munkatars_valid.js
@@ -1,17 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
 // 🔹 Változók a form elemeihez
-const form = document.getElementById("problemForm"); // signup vagy login form
-const username=document.getElementById("username");
-const email=document.getElementById("email");
-const password=document.getElementById("password");
-const status=document.getElementById("status");
+const form = document.getElementById("userForm") || document.getElementById("form"); // signup vagy login form
+const userFName = document.getElementById("fname");         // csak signup
+const userLName = document.getElementById("lname");         // csak signup
+const postCode = document.getElementById("postcode");     // csak signup
+const city = document.getElementById("city");             // csak signup
+const address = document.getElementById("address");       // csak signup
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");                 // mindkettő
+const password = document.getElementById("password");           // mindkettő
 const errorMessage = document.getElementById("error-message");
-
+const status = document.getElementById("status");           // csak signup
 // 🔹 Hibakereső függvények
-function getProblemFormErrors(usernameVal, emailVal, passwordVal, statusVal){
+function getNewUserFormErrors(userfnameVal,usersnameVal,postcodeVal, cityVal,addressVal, phoneVal, emailVal, passwordVal, statusVal){
     let errors = [];
 
-    if(!usernameVal) { errors.push("Szükséges a felhasználónév megadása!"); username.parentElement.classList.add("incorrect"); }
+    if(!userfnameVal) { errors.push("Szükséges a keresztnév megadása!"); userFName.parentElement.classList.add("incorrect"); }
+    if(!usersnameVal) { errors.push("Szükséges a vezetéknév megadása!"); userLName.parentElement.classList.add("incorrect"); }
     if(!emailVal)  { errors.push("Szükséges az email-cím megadása!"); email.parentElement.classList.add("incorrect"); }
     if(!passwordVal)   { errors.push("Szükséges a jelszó megadása!"); password.parentElement.classList.add("incorrect"); }
     if(!statusVal)   { errors.push("Szükséges a státusz megadása!"); status.parentElement.classList.add("incorrect"); }
@@ -22,7 +27,7 @@ function getProblemFormErrors(usernameVal, emailVal, passwordVal, statusVal){
 if(form){
     form.addEventListener('submit', async (e) => {
         let errors = [];
-        errors = getProblemFormErrors(username.value, email.value, password.value, status.value);
+        errors = getNewUserFormErrors(userFName?.value,postCode?.value,city?.value,address?.value, userLName?.value, phone?.value, email?.value, password?.value, status?.value);
 
         if(errors.length > 0){
             e.preventDefault();
@@ -38,9 +43,14 @@ if(form){
         }
 
         const formData = {
-        username: username.value,
-        email: email.value,
-        password: password.value,
+        userf: userFName?.value,
+        userl: userLName?.value,
+        postcode: postCode?.value,
+        city: city?.value,
+        address: address?.value,
+        email: email?.value,
+        phone: phone?.value, 
+        password: password?.value,
         status: status.value,
         };
 
@@ -71,7 +81,7 @@ if(form){
 }
 
 // 🔹 Inputok figyelése hibajelzés eltávolítására
-const allInputs = [username, email, password, status].filter(input => input != null);
+const allInputs = [userFName,userLName,postCode,city, address, phone, email, password, status].filter(input => input != null);
 allInputs.forEach(input => {
     input.addEventListener("input", () => {
         if(input.parentElement.classList.contains("incorrect")){
